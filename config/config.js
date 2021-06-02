e1bc37eb0b3cb2dfb9ef04fee7941173591aeb3e
Hikmet-dev <hikmetmekhdiev@gmail.com>
2021-06-02 21:54:26 +0300

    update config file 2

--- config/config.js
+++ config/config.js
@@ -1,6 +1,5 @@
 require('dotenv').config({ path: __dirname + '/.env' });
-console.log(require('dotenv').config());
-module.exports = {
+ module.exports = {
   "development": {
     "username": "postgres",
     "password": "437668820Asd",
@@ -17,12 +16,10 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_PROD_USER,
-    "password": process.env.DB_PROD_PASS,
-    "database": process.env.DB_PROD_DBNAME,
-    "host": process.env.DB_PROD_HOST,
-    "port": process.env.DB_PROD_PORT,
+    "use_env_variable": "postgres://dzhxdjumiixqtx:1a644464f35a9960fa2e88c71ab9a95a7aba264864181b8192d826db6b013d7b@ec2-54-155-226-153.eu-west-1.compute.amazonaws.com:5432/dcv2c1845oh2vs",
     "dialect": "postgres",
-    "use_env_variable": "postgres://dzhxdjumiixqtx:1a644464f35a9960fa2e88c71ab9a95a7aba264864181b8192d826db6b013d7b@ec2-54-155-226-153.eu-west-1.compute.amazonaws.com:5432/dcv2c1845oh2vs"
+    "dialectOptions": {
+      "ssl": true,
+    }   
   }
 }
