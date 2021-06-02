134dfe4882afdb73285ea2f1149f1faa5da217f6
Hikmet-dev <hikmetmekhdiev@gmail.com>
2021-06-02 22:01:04 +0300

    update config file 3

--- config/config.js
+++ config/config.js
@@ -16,10 +16,11 @@ require('dotenv').config({ path: __dirname + '/.env' });
     "dialect": "postgres"
   },
   "production": {
-    "use_env_variable": "postgres://dzhxdjumiixqtx:1a644464f35a9960fa2e88c71ab9a95a7aba264864181b8192d826db6b013d7b@ec2-54-155-226-153.eu-west-1.compute.amazonaws.com:5432/dcv2c1845oh2vs",
-    "dialect": "postgres",
-    "dialectOptions": {
-      "ssl": true,
-    }   
+    "username": process.env.DB_PROD_USER,
+    "password": process.env.DB_PROD_PASS,
+    "database": process.env.DB_PROD_DBNAME,
+    "host": process.env.DB_PROD_HOST,
+    "port": process.env.DB_PROD_PORT,
+    "dialect": "postgres"
   }
 }
