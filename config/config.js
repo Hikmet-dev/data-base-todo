5f3277a25166c6edcbf5bfbce96d543aff5e2d7f
Hikmet Mehdi-zade <hikmetmeh@gmail.com>
2021-06-02 17:42:36 +0300

    Update config file

--- config/config.js
+++ config/config.js
@@ -17,11 +17,6 @@ module.exports = {
     "dialect": "postgres"
   },
   "production": {
-    "username": process.env.DB_PROD_USER,
-    "password": process.env.DB_PROD_PASS,
-    "database": process.env.DB_PROD_DBNAME,
-    "host": process.env.DB_PROD_HOST,
-    "posr": process.env.DB_PROD_PORT,
-    "dialect": "postgres"
+    "use_env_variable": "postgres://dzhxdjumiixqtx:1a644464f35a9960fa2e88c71ab9a95a7aba264864181b8192d826db6b013d7b@ec2-54-155-226-153.eu-west-1.compute.amazonaws.com:5432/dcv2c1845oh2vs"
   }
 }
