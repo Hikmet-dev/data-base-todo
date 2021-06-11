1b90f76f01d448f428078f99956e601b8a4fe7fd
Hikmet Mehdi-zade <hikmetmeh@gmail.com>
2021-06-11 16:30:36 +0300

    Change filterBy  query req

--- routes/task/tasks.get.js
+++ routes/task/tasks.get.js
@@ -7,7 +7,7 @@ const errorMiddleware = require('../../middleware/errorMiddleware.js');
 
 
 router.get('/tasks',
-    query('filterBy').optional({checkFalsy: true}).isBoolean(),
+    query('filterBy').optional({checkFalsy: true}).isString().isIn(['all', 'done', 'undone']),
     query('order').isString().toUpperCase().isIn(['ASC', 'DESC']).optional({checkFalsy: true}),
     query('page').default(1).isInt(), 
     query('taskCount').default(100).isInt(),
@@ -17,10 +17,12 @@ router.get('/tasks',
         try{
             const { filterBy, order, taskCount, page}  = req.query;
             const {id} = res.locals.user;
+            
+            const filterParam = {'done': true, 'undone': false};
 
             const filter = { where: {user_id: id}};
             if (order && !'') filter.order = [["createdAt", order]];
-            if (!['', undefined].includes(filterBy)) filter.where.done = filterBy;
+            if (['done', 'undone'].includes(filterBy)) filter.where.done = filterParam[filterBy];
             filter.offset = (page - 1) * taskCount;
             filter.limit = taskCount;
 
