831cc5586e0e51d4fcbfebb8ef21499d8ba6cd52
Hikmet Mehdi-zade <hikmetmeh@gmail.com>
2021-06-18 18:00:49 +0300

    Add Constraint in Tasks migrations

--- migrations/20210604095716-add-new-columns.js
+++ migrations/20210604095716-add-new-columns.js
@@ -4,13 +4,23 @@
 // const User = user();
 module.exports = {
   up: async (queryInterface, DataTypes) => {
-    
    await queryInterface.addColumn('Tasks', 'user_id', {
     allowNull: false,  
     type: DataTypes.UUID,     
   }, {
     after: 'id'
  });
+ queryInterface.addConstraint('Tasks', {
+  fields: ['user_id'],
+  type: 'foreign key',
+  name: 'custom_fkey_constraint_user_id',
+  references: { //Required field
+    table: 'Users',
+    field: 'id'
+  },
+  onDelete: 'cascade',
+  onUpdate: 'cascade'
+ })
   },
   down: async (queryInterface, DataTypes) => {
     await queryInterface.removeColumn('Tasks', 'user_id')
